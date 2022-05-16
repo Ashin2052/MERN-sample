5582fb8a48e8131702a789a2f5f2a855f26f0219
Ashin2052 <mahat.ashin@gmail.com>
2022-05-16 22:13:52 +0545

    Added my project

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+import { json } from "body-parser";
+import express from "express";
+
+
+const app = express();
+
+app.use(json());
+app.listen(3000,()=>{
+
+    console.log("server is listening")
+});
