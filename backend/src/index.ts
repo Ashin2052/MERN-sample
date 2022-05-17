3180ae2cd1429a71cd02d8b2872f0a395cf9c24e
Ashin2052 <mahat.ashin@gmail.com>
2022-05-17 12:51:36 +0545

    connect to db

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,20 @@
 import { json } from "body-parser";
 import express from "express";
+import mongoose, { mongo } from "mongoose";
 
+require("dotenv").config({ path: __dirname + "/.env" });
 
 const app = express();
 
 app.use(json());
-app.listen(3000,()=>{
-
-    console.log("server is listening")
+app.listen(process.env.PORT, () => {
+  console.log("server is listening :" + process.env.PORT);
 });
+mongoose
+  .connect(process.env.URL || "mongodb://localhost/cms")
+  .then((result) => {
+    console.log("successfully conected to " + process.env.DB_NAME);
+  })
+  .catch((err) => {
+    console.log("ERROR:", err.message);
+  });
