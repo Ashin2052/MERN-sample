1cea01ef5b80d4b900ec2d5bba854db319750d46
Ashin2052 <mahat.ashin@gmail.com>
2022-06-06 13:51:02 +0545

    make file bug fixed

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,12 @@
 import { json } from "body-parser";
 import express from "express";
 import mongoose, { mongo } from "mongoose";
-import routes from './routes/routes';
+import routes from './src/routes/routes';
 
-require("dotenv").config({ path: __dirname + "/.env" });
+require("dotenv").config({ path:__dirname + "/config/dev.env" });
 
 const app = express();
-
+const connectionString = process.env.URL || "mongodb://mongo:27017/cms"
 app.use(json());
 app.listen(process.env.PORT, () => {
   console.log("server is listening :" + process.env.PORT);
@@ -14,7 +14,7 @@ app.listen(process.env.PORT, () => {
 app.use(routes);
 
 mongoose
-  .connect(process.env.URL || "mongodb://localhost/cms")
+  .connect(connectionString)
   .then((result) => {
     console.log("successfully conected to " + process.env.DB_NAME);
   })
